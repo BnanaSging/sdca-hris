c32d766a0001c41b69c198f31efd76eeb5879ab6
MajeranoL <majeranolian@gmail.com>
2026-06-07 23:39:31 +0800

    Added firebase dependencies

--- src/firebase.js
+++ src/firebase.js
@@ -5,13 +5,13 @@ import { getStorage } from "firebase/storage";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA2JYt0VAJwU5MVyYiz0mUcyM0wCsZJONg",
-  authDomain: "sdca-hris.firebaseapp.com",
-  projectId: "sdca-hris",
-  storageBucket: "sdca-hris.firebasestorage.app",
-  messagingSenderId: "665109477130",
-  appId: "1:665109477130:web:16c57a15468576f137acf3",
-  measurementId: "G-NWZX47B1WZ"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
